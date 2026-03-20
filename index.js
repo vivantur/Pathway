e5126e1cdd425faa901c12eaf741515a58712a3d
Olyvia <14greeno@gmail.com>
2026-03-20 14:22:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!hello') {
+    message.reply('Hey there! I am alive!');
+  }
+
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
