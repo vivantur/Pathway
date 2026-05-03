8b2fc3ba46ce2ffda8ac3b6d4507915fd9975035
Olyvia Green <14greeno@gmail.com>
2026-05-03 01:29:44 -0500

    Trim break holder by one space

--- index.js
+++ index.js
@@ -8044,7 +8044,7 @@ client.on('interactionCreate', async (interaction) => {
       await interaction.deferReply();
       const title = interaction.options.getString('title');
       if (!title) {
-        return await interaction.editReply(`\`${'\u2800'.repeat(84)}\``);
+        return await interaction.editReply(`\`${'\u2800'.repeat(83)}\``);
       }
       const discordSafeBlankHolderLine = '\u2800'.repeat(64);
       const blankHolderLine = '　'.repeat(64);
